3b50f821bb4511214a3b08633f72df58aa92a0dc
SusgUY446 <129160115+SusgUY446@users.noreply.github.com>
2023-11-14 19:06:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+# Imports
+
+import config
+import saves
+import threading
+
+# setup
+
+save_thread = threading.Thread(target=saves.save_game)
+
+
+save_thread.start()
+
+# main loop
+
+while True:
+    pass
+
+
+
+
 
